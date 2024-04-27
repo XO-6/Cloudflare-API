f4871cdcabbfdfcbf93cbb42a899029b480468fe
swnck <datapack3t.contact@gmail.com>
2024-04-27 20:40:42 +0200

    Added the first things

---
+++ build.gradle.kts
@@ -0,0 +1,50 @@
+plugins {
+    id("java-library")
+    id("maven-publish")
+}
+
+group = "studio.xo6"
+version = "1.0.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
+
+    compileOnly("org.projectlombok:lombok:1.18.30")
+    annotationProcessor("org.projectlombok:lombok:1.18.30")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            from(components["java"])
+
+            this.groupId = project.group.toString()
+            this.artifactId = project.name
+            this.version = project.version.toString()
+        }
+    }
+
+    repositories {
+        maven {
+            name = "nexus"
+            val isSnapshot = project.version.toString().endsWith("-SNAPSHOT")
+            url = if (isSnapshot)
+                uri("https://nexus.xo-6.studio/repository/maven-snapshots/")
+            else
+                uri("https://nexus.xo-6.studio/repository/maven-releases/")
+            credentials {
+                username = System.getenv("NEXUS_USERNAME")
+                password = System.getenv("NEXUS_PASSWORD")
+            }
+        }
+    }
+}
---
+++ src/main/java/studio/xo6/Bootstrap.java
@@ -0,0 +1,7 @@
+package studio.xo6;
+
+public class Bootstrap {
+    public static void main(String[] args) {
+
+    }
+}
